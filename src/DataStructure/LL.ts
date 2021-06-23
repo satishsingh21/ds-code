a9798317e87c4e6e99b9cd4ab489971897dd4d9c
Satish Singh <satishkumar.singh@triconinfotech.com>
2021-06-23 08:39:03 +0530

    ll get set insert added

--- src/DataStructure/LL.ts
+++ src/DataStructure/LL.ts
@@ -59,12 +59,85 @@ class SingleLinkedList {
         this.tail = newTail;
         this.length--;
 
-        // if there was only 1 item in list
+        // when list is empty
         if(this.length === 0){
             this.head = null;
             this.tail = null; 
         }
         return currentNode;
+    }
+
+    shift() {
+        // when list is empty
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+
+        // when list is empty
+        if(this.length === 0){
+            this.tail = null; 
+        }
+        return currentHead;
+    }
+
+    unshift(value){
+        let newNode  = new Node(value);
+        // when list is empty
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode['next'] = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            counter++;
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    set(index, value){
+        let foundNode = this.get(index)
+        if( foundNode) {
+            foundNode.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, value){
+        // if index is invalid
+        if(index < 0 || index > this.length) return false;
+
+        // if index is of size of list
+        if(index === this.length) return !!this.push(value);
+
+        // if index point to 0th element
+        if(index === 0) return !!this.unshift(value);
+
+        let newNode = new Node(value);
+
+        // if get previvoius node from the index 
+        let prevIndexNode = this.get(index - 1);
+
+        // swap
+        let temp = prevIndexNode.next;
+        prevIndexNode.next = newNode;
+        newNode['next'] = temp;
+        
+        this.length++
+        return true;
     }
 }
 
