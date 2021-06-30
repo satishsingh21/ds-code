334d3ee47f259c763179c248f6dd78d1262c3458
Satish Singh <satishkumar.singh@triconinfotech.com>
2021-06-30 08:48:09 +0530

    stack queue psedo code added

---
+++ src/DataStructure/queue.ts
@@ -0,0 +1,18 @@
+// queue has feature of FIFO
+
+// With array - push/shift is good approach as rebalance not required
+// Using liked list - enqueue/dequeue is good approach as we do not need to traverse to last node
+
+
+// Queue class
+class Queue
+{
+	// Array is used to implement a Queue
+	constructor()
+	{
+		this['items'] = [];
+	}
+				
+	// enqueue(item)
+	// dequeue()
+}
---
+++ src/DataStructure/stack.ts
@@ -0,0 +1,18 @@
+// Stack has feature of LIFO
+
+// With array - push/pop is good approach as rebalance not required
+// Using liked list - unshift/shift is good approach as we do not need to traverse to last node
+
+
+// Stack class
+class Stack {
+
+	// Array is used to implement stack
+	constructor()
+	{
+		this['items'] = [];
+	}
+
+	// push(item)
+	// pop()
+}
